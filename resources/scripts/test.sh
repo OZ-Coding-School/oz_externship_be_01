6a35ac73fd5d4cbff5d1b31c7111cf3b1cfb659f
meoyoug <eodyd426@gmail.com>
2025-06-19 17:13:51 +0900

    test.sh에 장고 env export 추가

--- resources/scripts/test.sh
+++ resources/scripts/test.sh
@@ -4,6 +4,8 @@ COLOR_GREEN=`tput setaf 2;`
 COLOR_BLUE=`tput setaf 4;`
 COLOR_NC=`tput sgr0;` # No Color
 
+export DJANGO_SETTINGS_MODULE=config.settings.local
+
 cd "$(dirname "$0")/../.."
 
 export DJANGO_SETTINGS_MODULE="config.settings.local"
