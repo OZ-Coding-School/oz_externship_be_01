e4bd079567f46b8e6944289fb4f1351af0f1215f
yeduen <gaurga@naver.com>
2025-06-20 20:25:03 +0900

    apps/models.py 정의

---
+++ apps/courses/models.py
@@ -0,0 +1,66 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+class Course(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    tag = models.CharField(max_length=3, unique=True)
+    description = models.CharField(max_length=255)
+    thumbnail_img_url = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'courses'
+
+
+class Generation(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="generations")
+    number = models.PositiveSmallIntegerField()
+    max_student = models.PositiveSmallIntegerField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    status = models.CharField(max_length=10)  # e.g., 'open', 'closed'
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ("course", "number")
+        db_table = "generations"
+
+    def __str__(self):
+        return f"{self.course.name} - {self.number}기"
+
+
+class Subject(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="subjects")
+    title = models.CharField(max_length=30, unique=True)
+    number_of_days = models.PositiveSmallIntegerField()
+    number_of_hours = models.PositiveSmallIntegerField()
+    thumbnail_img_url = models.URLField(unique=True)
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.course.name} - {self.title}"
+
+    class Meta:
+        db_table = "subjects"
+
+
+class EnrollmentRequest(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="enrollments")
+    generation = models.ForeignKey(Generation, on_delete=models.CASCADE, related_name="enrollments")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ("user", "generation")
+        db_table = "enrollment_requests"
+
+    def __str__(self):
+        return f"{self.user.name} requested to enrollment in  {self.generation}"
