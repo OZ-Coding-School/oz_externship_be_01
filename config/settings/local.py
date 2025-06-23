5725a497fc13538176896c9d45837bc3ea6fdac4
ryujaehak <jhryu627@gmail.com>
2025-06-23 13:25:04 +0900

    🐛 fix: local.py 복구

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,15 +1,6 @@
 from config.settings.base import *
-from decouple import Config, RepositoryEnv
-import os
 
 DEBUG = True
-
-env_path = os.path.join(BASE_DIR, "envs/.local.env")
-config = Config(repository=RepositoryEnv(env_path))
-
-SECRET_KEY = config("DJANGO_SECRET_KEY")
-DEBUG = config("DEBUG", default=False, cast=bool)
-
 ALLOWED_HOSTS = ["*"]
 
 # Static files (CSS, JavaScript, Images)
