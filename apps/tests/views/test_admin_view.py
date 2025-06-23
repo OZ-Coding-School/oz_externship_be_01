9f9247e25db88fb370d16fa9953f684c23c9ada1
ryujaehak <jhryu627@gmail.com>
2025-06-23 13:10:24 +0900

    ✨ feat     : git pull 전 커밋

--- apps/tests/views/test_admin_view.py
+++ apps/tests/views/test_admin_view.py
@@ -1,5 +1,7 @@
 from drf_spectacular.utils import extend_schema
 from rest_framework import status
+from rest_framework.decorators import permission_classes, api_view
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 
@@ -8,6 +10,14 @@ from rest_framework.response import Response
     description="관리자 또는 스태프가 등록한 쪽지시험 문제를 Hard Delete 방식으로 삭제합니다.",
     responses={
         204: None,
+        403: {"세부 정보 : 이 작업을 수행할 권한이 없습니다."},
         404: {"세부 정보 : 테스트 질문을 찾을 수 없습니다."}
     }
 )
+@api_view(["DELETE"])
+@permission_classes([IsAuthenticated])
+def delete_test_view(request, question_id):
+    user = request.user
+    if not user.is_superuser and user.role not in ['STAFF', 'COACH']:
+        return Response({'세부 정보': '이 작업을 수행할 권한이 없습니다.'},
+                        status=status.HTTP_403_FORBIDDEN)
