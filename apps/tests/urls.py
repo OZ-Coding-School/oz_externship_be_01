0d77a2b273886395a8c380f1017997ce35716f3a
ryujaehak <jhryu627@gmail.com>
2025-06-24 15:33:35 +0900

    ✨ feat: 쪽지시험 문제(조회,등록,수정,삭제) Mock 작성
    # Conflicts:
    #	config/urls.py
    
    # Conflicts:
    #	apps/tests/urls.py

--- apps/tests/urls.py
+++ apps/tests/urls.py
@@ -8,6 +8,13 @@ from .views.admin_test_views import (
     AdminTestUpdateAPIView,
 )
 
+from .views.test_admin_view import (
+    create_test_questions,
+    delete_test_questions,
+    list_test_questions,
+    update_test_questions,
+)
+
 app_name = "tests"
 
 urlpatterns = [
@@ -16,4 +23,8 @@ urlpatterns = [
     path("admin/tests/<int:test_id>/", AdminTestDetailAPIView.as_view(), name="test-detail"),
     path("admin/tests/", AdminTestListView.as_view(), name="admin-test-list"),
     path("admin/tests/create", AdminTestCreateAPIView.as_view(), name="test-create"),
+    path("test-questions/", create_test_questions, name="create-test-questions"),
+    path("test-questions/<int:question_id>/", update_test_questions, name="update-test-questions"),
+    path("test-questions/<int:question_id>/delete/", delete_test_questions, name="delete-test-questions"),
+    path("test-questions/list/", list_test_questions, name="list-test-questions"),
 ]
