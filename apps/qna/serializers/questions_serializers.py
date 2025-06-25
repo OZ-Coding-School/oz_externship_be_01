1419da5d49774e3449783076fb17d8118d577a16
“leejiyun1” <poi20701556@gmail.com>
2025-06-25 21:30:26 +0900

    💡 chore    : 멘토님을 위한 커밋

--- apps/qna/serializers/questions_serializers.py
+++ apps/qna/serializers/questions_serializers.py
@@ -7,53 +7,30 @@ from apps.qna.models import Question, QuestionCategory
 
 # 1. 질문 생성
 class QuestionCreateSerializer(serializers.ModelSerializer[Question]):
-    image_urls = serializers.ListField(child=serializers.URLField(), write_only=True, required=False)
+    image_urls = serializers.ListField(child=serializers.URLField(), read_only=True)
+    image_files = serializers.ListField(child=serializers.FileField(), write_only=True, required=False)
+    category_id = serializers.IntegerField(write_only=True)
 
     class Meta:
         model = Question
-        fields = ["category_id", "title", "content", "image_urls"]
-
-    def validate_category_id(self, value: int) -> int:
-        try:
-            cat = QuestionCategory.objects.get(id=value)
-        except QuestionCategory.DoesNotExist:
-            raise serializers.ValidationError("소분류 카테고리만 선택 가능합니다.")
-        if not (cat.parent and cat.parent.parent):
-            raise serializers.ValidationError("소분류 카테고리만 선택 가능합니다.")
-        return value
-
-    def validate_image_urls(self, value: List[str]) -> List[str]:
-        if len(value) > 5:
-            raise serializers.ValidationError("이미지는 최대 5개까지 업로드할 수 있습니다.")
-        return value
+        fields = ["category", "category_id", "title", "content", "image_urls", "image_files"]
+        read_only_fields = ["category"]
 
 
 # 2. 질문 수정
 class QuestionUpdateSerializer(serializers.ModelSerializer[Question]):
-    image_urls = serializers.ListField(child=serializers.URLField(), write_only=True, required=False)
+    image_urls = serializers.ListField(child=serializers.URLField(), read_only=True)
+    image_files = serializers.ListField(child=serializers.FileField(), write_only=True, required=False)
+    category_id = serializers.IntegerField(required=False, write_only=True)
 
     class Meta:
         model = Question
-        fields = ["title", "content", "category_id", "image_urls"]
+        fields = ["category", "category_id", "title", "content", "image_urls", "image_files"]
+        read_only_fields = ["category"]
         extra_kwargs = {
             "title": {"required": False},
             "content": {"required": False},
-            "category_id": {"required": False},
         }
-
-    def validate_category_id(self, value: int) -> int:
-        try:
-            cat = QuestionCategory.objects.get(id=value)
-        except QuestionCategory.DoesNotExist:
-            raise serializers.ValidationError("소분류 카테고리만 선택 가능합니다.")
-        if not (cat.parent and cat.parent.parent):
-            raise serializers.ValidationError("소분류 카테고리만 선택 가능합니다.")
-        return value
-
-    def validate_image_urls(self, value: List[str]) -> List[str]:
-        if len(value) > 5:
-            raise serializers.ValidationError("이미지는 최대 5개까지 업로드할 수 있습니다.")
-        return value
 
 
 # 3. 질문 목록 조회
