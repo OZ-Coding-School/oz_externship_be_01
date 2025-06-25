045b5a261d5e12ad1f4bc5afd785e78d8fbb117e
choismjames23 <choism23@naver.com>
2025-06-25 10:43:02 +0900

    ✨ feat: 질의응답 answer 부분 mock api 작
    answer 관련 serializer를 매우 간단하게 작성하고, answer view를 그에 맞춰 응답만 하는 형식으로 작성.
    이후 도커 환경에서 스웨거 문서에 잘 나오는 것을 확인.

---
+++ apps/qna/serializers/answers_serializers.py
@@ -0,0 +1,54 @@
+from typing import Any
+
+from rest_framework import serializers
+from apps.qna.models import Answer, AnswerImage, AnswerComment
+
+
+class AnswerImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AnswerImage
+        fields = ["img_url"]
+
+
+class AnswerCreateSerializer(serializers.ModelSerializer):
+    images = serializers.ListField(
+        child=serializers.ImageField(), write_only=True, required=False
+    )
+
+    class Meta:
+        model = Answer
+        fields = ["content", "images"]
+
+    def create(self, validated_data: dict[str, Any]) -> Answer:
+        images = validated_data.pop("images", [])
+        # answer 모델에 직접 이미지를 저장하지 않기 때문에 validate_data에서 일단 꺼내서 answer create
+        answer = Answer.objects.create(**validated_data)
+        return answer
+
+
+class AnswerUpdateSerializer(serializers.ModelSerializer):
+    images = serializers.ListField(
+        child=serializers.ImageField(), write_only=True, required=False
+    )
+    delete_image_ids = serializers.CharField(required=False)
+
+    class Meta:
+        model = Answer
+        fields = ["content", "images", "delete_image_ids"]
+
+    def update(self, instance: Answer, validated_data: dict[str, Any]) -> Answer:
+        # 컨텐츠 업데이트만 구현, 이미지 수정, 삭제는 나중에
+        instance.content = validated_data.get("content", instance.content)
+        instance.save()
+        return instance
+
+
+class AnswerCommentCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AnswerComment
+        fields = ["content"]
+
+    def validate_content(self, value: str) -> str:
+        if len(value) > 500:
+            raise serializers.ValidationError("댓글 내용은 500자 이하로 입력해야 합니다.")
+        return value
---
+++ apps/qna/views/answers_views.py
@@ -0,0 +1,47 @@
+from rest_framework.request import Request
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
+
+from apps.qna.models import Question, Answer
+from apps.qna.serializers.answers_serializers import (
+    AnswerCreateSerializer,
+    AnswerUpdateSerializer,
+    AnswerCommentCreateSerializer,
+)
+
+class AnswerCreateView(APIView):
+    serializer_class = AnswerCreateSerializer
+
+    def post(self, request: Request, question_id: int) -> Response:
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class AnswerUpdateView(APIView):
+    serializer_class = AnswerUpdateSerializer
+
+    def put(self, request: Request, answer_id: int) -> Response:
+        answer = get_object_or_404(Answer, id=answer_id)
+        serializer = self.serializer_class(instance=answer, data=request.data)
+        if serializer.is_valid():
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class AdoptAnswerView(APIView):
+    def post(self, request: Request, question_id: int, answer_id: int) -> Response:
+        return Response({"message": "OK"}, status=status.HTTP_200_OK)
+
+
+class AnswerCommentCreateView(APIView):
+    serializer_class = AnswerCommentCreateSerializer
+
+    def post(self, request: Request, answer_id: int) -> Response:
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
