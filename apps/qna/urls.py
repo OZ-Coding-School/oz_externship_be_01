7b0440728bcba99c5090917deb6dfb1d2a1f26d3
hoon <eoeo4652@naver.com>
2025-06-26 14:20:10 +0900

    ✨ feat: admin mock api 작성
    -질의응답 카테고리 등록 apps/qna/admin_serializers.py/apps/qna/admin_views.py
    -질의응답 카테고리 삭제 apps/qna/admin_serializers.py/apps/qna/admin_views.py
    -질문 삭제 apps/qna/admin_serializers.py/apps/qna/admin_views.py
    -답변 삭제 apps/qna/amdin_seriaelizer.py/apps/qna/admin_veiws.py
    -urls.py 작성
    (admin)mock api 작성.
    제

--- apps/qna/urls.py
+++ apps/qna/urls.py
@@ -1,1 +1,17 @@
+from django.urls import path
+
+from apps.qna.views.admin_views import (
+    AdminCategoryCreateView,
+    AdminCategoryDeleteView,
+    AdminQuestionDeleteView,
+    AdminAnswerDeleteView
+)
+
 app_name = "qna"
+
+urlpatterns = [
+    path("admin/categories/create/", AdminCategoryCreateView.as_view(), name="admin-category-create"),
+    path("admin/categories/delete/", AdminCategoryDeleteView.as_view(), name="admin-category-delete"),
+    path("admin/questions/delete/", AdminQuestionDeleteView.as_view(), name="admin-question-delete"),
+    path("admin/answers/delete/", AdminAnswerDeleteView.as_view(), name="admin-answer-delete"),
+]
