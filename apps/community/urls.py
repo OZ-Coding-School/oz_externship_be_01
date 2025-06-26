2f2d454464be18facf291b78f84e18d459e9b3e2
yeon tae <g9060g9060@gmail.com>
2025-06-26 18:22:00 +0900

    ✨ feat:migrations 생성

--- apps/community/urls.py
+++ apps/community/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+
 from apps.community.views.admin.comment_views import AdminCommentDeleteAPIView
 from apps.community.views.admin.notice_views import NoticeCreateAPIView
 from apps.community.views.user.comment_views import CommentListAPIView
 
 urlpatterns = [
     path("admin/notices/", NoticeCreateAPIView.as_view(), name="admin-notice"),
-    path("comments/<int:comment_id>/", AdminCommentDeleteAPIView.as_view()),  
+    path("comments/<int:comment_id>/", AdminCommentDeleteAPIView.as_view()),
     path("posts/<int:post_id>/comments/", CommentListAPIView.as_view(), name="comment-list"),
 ]
