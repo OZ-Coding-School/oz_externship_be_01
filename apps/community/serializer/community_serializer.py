641649ae92b8f2cb644182bcd26b5729bd129f9c
yeon tae <g9060g9060@gmail.com>
2025-06-26 11:33:03 +0900

    ♻️ refactor: 중복 폴더 제거

--- apps/community/serializer/community_serializer.py
+++
@@ -1,7 +0,0 @@
-from typing import Any, Dict
-
-from rest_framework import serializers
-
-
-class CommentDeleteResponseSerializer(serializers.Serializer[Dict[str, Any]]):
-    detail = serializers.CharField(help_text="성공 또는 실패 메시지")
