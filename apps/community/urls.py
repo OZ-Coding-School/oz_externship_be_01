8b46b3d943c0a9848ba00a743413fb84590b9e8b
enjore <shawn630730@gmail.com>
2025-06-26 11:35:15 +0700

    🐛 fix      :  댓글 조히 views 수정

--- apps/community/urls.py
+++ apps/community/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
+from apps.community.views.user.comment_views import CommentListAPIView
 from apps.community.views.admin.notice_views import NoticeCreateAPIView
 
 urlpatterns = [
+    path("posts/<int:post_id>/comments/", CommentListAPIView.as_view(), name="comment-list"),
     path("admin/notices/", NoticeCreateAPIView.as_view(), name="admin-notice"),  # 공지사항
 ]
