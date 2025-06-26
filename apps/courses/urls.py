02f2d8d3f4a08204b237f558cb9071d725013eaa
muk <lhm4802@nate.com>
2025-06-26 11:36:23 +0900

    ✨ feat     : add subject Mock API for admin
    # Conflicts:
    #	apps/courses/urls.py
    #	config/urls.py

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 
 from .views.course_views import CourseDetailView, CourseListCreateView
+from apps.courses.views.subject_views import (
+    SubjectDetailAPIView,
+    SubjectListCreateAPIView,
+)
+
 
 urlpatterns = [
-    path("", CourseListCreateView.as_view(), name="v1_admin_course_list"),
-    path("<int:course_id>/", CourseDetailView.as_view(), name="v1_admin_course_detail"),
+    path("courses/", CourseListCreateView.as_view(), name="v1_admin_course_list"),
+    path("courses/<int:course_id>/", CourseDetailView.as_view(), name="v1_admin_course_detail"),
+    path("subjects/", SubjectListCreateAPIView.as_view(), name="subject-list-create"),
+    path("subjects/<int:subject_id>/", SubjectDetailAPIView.as_view(), name="subject-detail"),
 ]
