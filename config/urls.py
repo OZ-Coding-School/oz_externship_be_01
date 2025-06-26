a29216ebbe82f3cdfe2390dc105ed6e271882d5f
muk <lhm4802@nate.com>
2025-06-26 11:36:23 +0900

    ✨ feat     : add subject Mock API for admin
    # Conflicts:
    #	apps/courses/urls.py
    #	config/urls.py

--- config/urls.py
+++ config/urls.py
@@ -9,7 +9,7 @@ from drf_spectacular.views import (
 urlpatterns: list[URLPattern | URLResolver] = [
     path("api/v1/qna/", include("apps.qna.urls")),
     path("api/v1/community/", include("apps.community.urls")),
-    path("api/v1/admin/course/", include("apps.courses.urls")),
+    path("api/v1/admin/", include("apps.courses.urls")),
     path("api/v1/", include("apps.users.urls")),
 ]
 
