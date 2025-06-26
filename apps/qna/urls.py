ede6f6b7cd379ddd9cd601e747a93b3c518c1d80
hoon <eoeo4652@naver.com>
2025-06-26 17:59:02 +0900

    ✨ feat: admin mock api 작성
    -질의응답 카테고리 등록 apps/qna/admin_serializers.py/apps/qna/admin_views.py
    -질의응답 카테고리 삭제 apps/qna/admin_serializers.py/apps/qna/admin_views.py
    -질문 삭제 apps/qna/admin_serializers.py/apps/qna/admin_views.py
    -답변 삭제 apps/qna/amdin_seriaelizer.py/apps/qna/admin_veiws.py
    -urls.py 작성
    (admin)mock api 작성.
    제
    -qna/urls.py 충돌 병합.
    
    # Conflicts:
    #	apps/qna/urls.py

--- apps/qna/urls.py
+++ apps/qna/urls.py
@@ -1,11 +1,20 @@
 from django.urls import path
 
+
 from apps.qna.views.answers_views import (
     AdoptAnswerView,
     AnswerCommentCreateView,
     AnswerCreateView,
-    AnswerUpdateView,
+    AnswerUpdateView
 )
+
+from apps.qna.views.admin_views import (
+    AdminCategoryCreateView,
+    AdminCategoryDeleteView,
+    AdminQuestionDeleteView,
+    AdminAnswerDeleteView
+)
+
 from apps.qna.views.questions_views import (
     QuestionCreateView,
     QuestionDetailView,
@@ -14,6 +23,7 @@ from apps.qna.views.questions_views import (
 )
 
 urlpatterns = [
+
     # questions
     path("questions/", QuestionListView.as_view(), name="question-list"),
     path("questions/create/", QuestionCreateView.as_view(), name="question-create"),
@@ -24,4 +34,9 @@ urlpatterns = [
     path("questions/<int:question_id>/answers/<int:answer_id>/", AnswerUpdateView.as_view()),
     path("questions/<int:question_id>/answers/<int:answer_id>/adopt/", AdoptAnswerView.as_view()),
     path("questions/answers/<int:answer_id>/comments/", AnswerCommentCreateView.as_view()),
+    # admin
+    path("admin/categories/create/", AdminCategoryCreateView.as_view(), name="admin-category-create"),
+    path("admin/categories/delete/", AdminCategoryDeleteView.as_view(), name="admin-category-delete"),
+    path("admin/questions/delete/", AdminQuestionDeleteView.as_view(), name="admin-question-delete"),
+    path("admin/answers/delete/", AdminAnswerDeleteView.as_view(), name="admin-answer-delete"),
 ]
