17d01d89e876baa9bccb0b7ef55bee4e8155f9f3
kyukyu300 <kyujin1252@gmail.com>
2025-06-26 23:32:40 +0900

    💡 chore: /admin prefix 추가

--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,5 @@ if settings.DEBUG:
             path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
             path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
             path("api/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
-            path("api/v1/admin/", include("apps.users.urls")),
+            path("api/v1/", include("apps.users.urls")),
         ]
