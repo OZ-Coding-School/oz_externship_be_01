2fb3072f37efae27c9537f9c3f2c705225f8d486
enjore <shawn630730@gmail.com>
2025-06-26 16:21:57 +0700

    ✨  feat     :  댓글 작성 기능 추가 mockAPI

--- apps/community/serializers/comment_serializer.py
+++ apps/community/serializers/comment_serializer.py
@@ -28,3 +28,16 @@ class CommentResponseSerializer(serializers.ModelSerializer[Any]):
             "updated_at",
         )
         read_only_fields = ("created_at", "updated_at")
+
+
+class CommentCreateSerializer(serializers.ModelSerializer[Any]):
+    content = serializers.CharField(max_length=255)
+
+    def validate(self, value: Any) -> Any:
+        if not value:
+            raise serializers.ValidationError("내용이 비어 있습니다.")
+        return value
+
+    class Meta:
+        model = Comment
+        fields = ["content"]
--- apps/community/urls.py
+++ apps/community/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 
 from apps.community.views.admin.comment_views import AdminCommentDeleteAPIView
 from apps.community.views.admin.notice_views import NoticeCreateAPIView
-from apps.community.views.user.comment_views import CommentListAPIView
+from apps.community.views.user.comment_views import CommentListAPIView, CommentCreateAPIView
 
 urlpatterns = [
     path("admin/notices/", NoticeCreateAPIView.as_view(), name="admin-notice"),
     path("comments/<int:comment_id>/", AdminCommentDeleteAPIView.as_view()),
     path("posts/<int:post_id>/comments/", CommentListAPIView.as_view(), name="comment-list"),
+    path("posts/<int:post_id>/comments/create/", CommentCreateAPIView.as_view(), name="comment-create"),
 ]
