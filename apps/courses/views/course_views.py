4814c3273d995f20b35799091fc7b50557da6fd3
yeduen <gaurga@naver.com>
2025-06-26 17:22:34 +0900

    [Style] isort import 정렬 적용

--- apps/courses/views/course_views.py
+++ apps/courses/views/course_views.py
@@ -6,8 +6,8 @@ from rest_framework.permissions import AllowAny
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from apps.courses.models import Course
 
+from apps.courses.models import Course
 from apps.courses.serializers.course_serializers import (
     CourseListSerializer,
     CourseSerializer,
