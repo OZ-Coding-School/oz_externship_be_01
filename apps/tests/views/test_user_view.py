1513a5f595f0e9fdec2399bae392fe3f66ebdbf8
leemera <jseejy57@gmail.com>
2025-06-27 10:08:00 +0900

    ✅ 오류 수정

--- apps/tests/views/test_user_view.py
+++ apps/tests/views/test_user_view.py
@@ -1,7 +1,7 @@
 from drf_spectacular.utils import extend_schema
 from rest_framework import status
-from rest_framework.response import Response
 from rest_framework.request import Request
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from apps.tests.serializers.user_admin_deployment_serializers import (
