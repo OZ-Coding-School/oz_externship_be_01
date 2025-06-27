67b05941527e825b9ee0e334a54695f9fc0b070c
harry99990 <xornjs9502@naver.com>
2025-06-27 18:13:20 +0900

    🐛 fix: 코드 리팩토링
    2. 변경 또는 추가사항을 아래에 간략하게 작성하세요 ( 필수 )
    email_login 데코레이터 수정

--- apps/users/views/auth/email_login.py
+++ apps/users/views/auth/email_login.py
@@ -2,11 +2,16 @@ from rest_framework import status
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from drf_spectacular.utils import extend_schema
 
 from apps.users.serializers.auth.email_login import EmailLoginSerializer
 
 
 class EmailLoginAPIView(APIView):
+    @extend_schema(
+        request=EmailLoginSerializer,
+        responses={200: None, 400: None},
+        tags=["auth"],)
     def post(self, request: Request) -> Response:
         serializer = EmailLoginSerializer(data=request.data)
         if serializer.is_valid():
