a81a7ef1daba4df48110a3bc27b896b181ab3326
hoon <eoeo4652@naver.com>
2025-06-29 16:14:14 +0900

    ✨ feat: qna_admin 파일 작성
    -admin 답변 삭제
    -admin 질문 삭제
    -admin 질의응답 상세 조회
    -admin 질문 목록 조회
    -admin 질의응답 카테고리 삭제
    -admin 질의응답 카테고리 목록 조회
    -admin 질의응답 카테고리 등록
    (admin) mock api 코드 작성

--- apps/qna/serializers/admin_serializers.py
+++ apps/qna/serializers/admin_serializers.py
@@ -1,67 +1,51 @@
-from typing import Any
-
 from rest_framework import serializers
 
-from apps.qna.models import Answer, Question, QuestionCategory
+from apps.qna.models import Question, QuestionCategory, QuestionImage
+
+
+# 질문 이미지
+class AdminQuestionImageSerializer(serializers.ModelSerializer[QuestionImage]):
+    class Meta:
+        model = QuestionImage
+        fields = ["id", "img_url", "created_at", "updated_at"]
+        read_only_fields = fields
+
+
+# 질문 목록 조회
+class AdminQuestionListSerializer(serializers.ModelSerializer[Question]):
+    images = AdminQuestionImageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Question
+        fields = ["id", "title", "content", "author", "category", "view_count", "created_at", "updated_at", "images"]
+
+
+# 질문 상세 조회
+class AdminQuestionDetailSerializer(serializers.ModelSerializer[Question]):
+    images = AdminQuestionImageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Question
+        fields = ["id", "title", "content", "author", "category", "view_count", "created_at", "updated_at", "images"]
 
 
 # 질의응답 카테고리 질문 등록
 class AdminCategoryCreateSerializer(serializers.ModelSerializer[QuestionCategory]):
+    type = serializers.CharField(write_only=True, required=True)
+
     class Meta:
         model = QuestionCategory
-        fields = ["id", "name", "parent", "created_at", "updated_at"]
+        fields = ["id", "name", "parent", "type", "created_at", "updated_at"]
+        read_only_fields = ["id", "created_at", "updated_at"]
 
-    def validate(self, data: dict[str, Any]) -> dict[str, Any]:
-        if data.get("type") == "대" and data.get("parent_id"):
-            raise serializers.ValidationError("대분류는 parent_id를 지정할 수 없습니다.")
-        if data.get("type") != "대" and not data.get("parent_id"):
-            raise serializers.ValidationError("중분류 또는 소분류는 parent_id를 지정해야 합니다.")
-        return data
-
-    def create(self, validated_data: dict[str, Any]) -> QuestionCategory:
-        category = QuestionCategory.objects.create(**validated_data)
-        return category
+    # 추후 카테고리 검증 코드 추가 예정
+    # 검증 된 데이터로 생성 코드 구현 예정
 
 
-# 질의응답 카테고리 질문 삭제
-class AdminCategoryDeleteResponseSerializer(serializers.ModelSerializer[QuestionCategory]):
-    category_id = serializers.IntegerField()
-    detail = serializers.SerializerMethodField()
+# 카테고리 목록 조회
+class AdminCategoryListSerializer(serializers.ModelSerializer[Question]):
+    images = AdminQuestionImageSerializer(many=True, read_only=True)
 
-    def validate(self, data: dict[str, Any]) -> dict[str, Any]:
-        try:
-            QuestionCategory.objects.get(id=data["category_id"])
-        except QuestionCategory.DoesNotExist:
-            raise serializers.ValidationError("존재하지 않는 질문입니다.")
-        return data
-
-
-# 답변 삭제
-class AdminAnswerDeleteSerializer(serializers.ModelSerializer[Answer]):
-    answer_id = serializers.IntegerField()
-    detail = serializers.CharField()
-
-    def validate(self, data: dict[str, Any]) -> dict[str, Any]:
-        try:
-            Answer.objects.get(id=data["answer_id"])
-        except Answer.DoesNotExist:
-            raise serializers.ValidationError("존재하지 않는 답변입니다.")
-        return data
-
-
-# 질문 삭제
-class AdminQuestionDeleteSerializer(serializers.ModelSerializer[Question]):
-    question_id = serializers.IntegerField()
-    detail = serializers.CharField()
-
-    def validate(self, data: dict[str, Any]) -> dict[str, Any]:
-        try:
-            Question.objects.get(id=data["question_id"])
-        except Question.DoesNotExist:
-            raise serializers.ValidationError("존재하지 않는 질문입니다.")
-        return data
-
-
-# 질의응답 상세 조회
-# 질문 목록 조회
-# 징의응답 카테고리 목록 조회
+    class Meta:
+        model = Question
+        fields = ["id", "name", "parent", "type", "created_at", "updated_at"]
--- apps/qna/views/admin_views.py
+++ apps/qna/views/admin_views.py
@@ -1,74 +1,132 @@
+from typing import Any
+
 from drf_spectacular.utils import extend_schema
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from apps.qna.models import Answer, Question, QuestionCategory
+from apps.qna.dummy import dummy
 from apps.qna.serializers.admin_serializers import (
-    AdminAnswerDeleteSerializer,
-    AdminCategoryCreateSerializer,
-    AdminCategoryDeleteResponseSerializer,
-    AdminQuestionDeleteSerializer,
+    AdminCategoryListSerializer,
+    AdminQuestionDetailSerializer,
+    AdminQuestionImageSerializer,
+    AdminQuestionListSerializer,
 )
 
 
-# 질의응답 카테고리 등록
+# 카테고리 등록
 class AdminCategoryCreateView(APIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
 
-    @extend_schema(tags=["(Admin) 질문 카테고리 등록"])
-    def post(self, request: Request) -> Response:
+    @extend_schema(
+        tags=["(Admin) QnA"],
+        description="등록할 카테고리 ID",
+        summary="카테고리 등록",
+    )
+    def post(self, request: Request, category_id: int) -> Response:
+        serializer = AdminCategoryListSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(category_id=category_id)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-        serializer = AdminCategoryCreateSerializer(data=request.data)
-        if serializer.is_valid():
-            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# 카테고리 목록 조회
+class AdminCategoryListView(APIView):
+    permission_classes = [AllowAny]
+
+    @extend_schema(
+        tags=["(Admin) QnA"],
+        description="조회할 카테고리 ID",
+        summary="카테고리 목록 조회",
+    )
+    def get(self, request: Request, *args: Any, **kwargs: Any) -> Response:
+        serializer = AdminCategoryListSerializer(dummy.DUMMY_QUESTIONS, many=True)
+        resp_data = serializer.data
+        for data in resp_data:
+            images = [image for image in dummy.DUMMY_QUESTION_IMAGES if data["id"] == image.question.id]
+            data["images"] = AdminQuestionImageSerializer(images, many=True).data
+        return Response(resp_data, status=status.HTTP_200_OK)
 
 
-# 질의응답 카테고리 삭제
+# 카테고리 삭제
 class AdminCategoryDeleteView(APIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
 
-    @extend_schema(tags=["(Admin) 질문 카테고리 삭제"])
+    @extend_schema(
+        tags=["(Admin) QnA"],
+        description="삭제할 카테고리 ID",
+        summary="카테고리 삭제",
+    )
     def delete(self, request: Request, category_id: int) -> Response:
-        serializer = AdminCategoryDeleteResponseSerializer(data=request.data)
-        if serializer.is_valid():
-            category = QuestionCategory.objects.get(id=category_id)
-            category.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        if not any(q.id == category_id for q in dummy.DUMMY_QUESTIONS):
+            return Response({"detail": "해당 카테고리가 존재하지 않습니다."}, status=status.HTTP_404_NOT_FOUND)
+
+        # 원래는 이런 방식이 아닌데 더미라서 orm을 사용할 수 없기에 이렇게 합니다.
+        dummy.DUMMY_CATEGORY = [q for q in dummy.DUMMY_CATEGORY if q.id != category_id]
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-# 답변 삭제
-class AdminAnswerDeleteView(APIView):
-    permission_classes = [IsAuthenticated]
+# 질의응답 상세 조회
+class AdminQnaDetailView(APIView):
+    permission_classes = [AllowAny]
 
-    @extend_schema(tags=["(Admin) 답변 삭제"])
-    def delete(self, request: Request, answer_id: int) -> Response:
-        serializer = AdminAnswerDeleteSerializer(data=request.data)
-        if serializer.is_valid():
-            answer = Answer.objects.get(id=answer_id)
-            answer.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    @extend_schema(tags=["(Admin) QnA"], description="조회할 질의응답 ID", summary="질의응답 상세 조회")
+    def get(self, request: Request, question_id: int, *args: Any, **kwargs: Any) -> Response:
+        # 1. 질문 찾기
+        item = next((q for q in dummy.DUMMY_QUESTIONS if q.id == question_id), None)
+        if not item:
+            return Response({"detail": "해당 질문이 존재하지 않습니다."}, status=status.HTTP_404_NOT_FOUND)
+
+        # 2. 직렬화
+        serializer = AdminQuestionDetailSerializer(item)
+        data = serializer.data
+
+        # 3. 관련 이미지 mock 추가
+        images = [
+            img for img in dummy.DUMMY_QUESTION_IMAGES if getattr(img.question, "id", img.question) == question_id
+        ]
+        data["images"] = AdminQuestionImageSerializer(images, many=True).data
+
+        return Response(data, status=status.HTTP_200_OK)
+
+
+# 질문 목록 조회
+class AdminQuestionListView(APIView):
+    permission_classes = [AllowAny]
+
+    @extend_schema(tags=["(Admin) QnA"], description="조회할 질문 ID", summary="질문 목록 조회")
+    def get(self, request: Request, *args: Any, **kwargs: Any) -> Response:
+        serializer = AdminQuestionListSerializer(dummy.DUMMY_QUESTIONS, many=True)
+        resp_data = serializer.data
+        for data in resp_data:
+            images = [image for image in dummy.DUMMY_QUESTION_IMAGES if data["id"] == image.question.id]
+            data["images"] = AdminQuestionImageSerializer(images, many=True).data
+        return Response(resp_data, status=status.HTTP_200_OK)
 
 
 # 질문 삭제
 class AdminQuestionDeleteView(APIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
 
-    @extend_schema(tags=["(Admin) 질문 삭제"])
+    @extend_schema(tags=["(Admin) QnA"], description="삭제할 질문 ID", summary="질문 삭제")
     def delete(self, request: Request, question_id: int) -> Response:
-        serializer = AdminQuestionDeleteSerializer(data=request.data)
-        if serializer.is_valid():
-            question = Question.objects.get(id=question_id)
-            question.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        if not any(q.id == question_id for q in dummy.DUMMY_QUESTIONS):
+            return Response({"detail": "해당 질문이 존재하지 않습니다."}, status=status.HTTP_404_NOT_FOUND)
+        # 원래는 이런 방식이 아닌데 더미라서 orm을 사용할 수 없기에 이렇게 합니다.
+        dummy.DUMMY_QUESTIONS = [q for q in dummy.DUMMY_QUESTIONS if q.id != question_id]
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-# 질의응답 상세 조회
-# 질문 목록 조회
-# 징의응답 카테고리 목록 조회
+# 답변 삭제
+class AdminAnswerDeleteView(APIView):
+    permission_classes = [AllowAny]
+
+    @extend_schema(tags=["(Admin) QnA"], description="삭제할 답변 ID", summary="답변 삭제")
+    def delete(self, request: Request, answer_id: int) -> Response:
+        if not any(q.id == answer_id for q in dummy.DUMMY_ANSWERS):
+            return Response({"detail" "해당 답변이 존재하지 않습니다."}, status=status.HTTP_404_NOT_FOUND)
+        # 원래는 이런 방식이 아닌데 더미라서 orm을 사용할 수 없기에 이렇게 합니다.
+        dummy.DUMMY_ANSWERS = [q for q in dummy.DUMMY_ANSWERS if q.id != answer_id]
+        return Response(status=status.HTTP_204_NO_CONTENT)
