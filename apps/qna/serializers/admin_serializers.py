b70c6aa173752411f79359c9947ae40fc1b1ba1d
“leejiyun1” <poi20701556@gmail.com>
2025-06-29 17:47:05 +0900

    🐛 fix      : 오류 수정 및 질의 응답 상세조회 api 수정

--- apps/qna/serializers/admin_serializers.py
+++ apps/qna/serializers/admin_serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from apps.qna.models import Question, QuestionCategory, QuestionImage
+from apps.qna.models import Question, QuestionCategory, QuestionImage, Answer
 
 
 # 질문 이미지
@@ -9,6 +9,19 @@ class AdminQuestionImageSerializer(serializers.ModelSerializer[QuestionImage]):
         model = QuestionImage
         fields = ["id", "img_url", "created_at", "updated_at"]
         read_only_fields = fields
+
+class AdminAnswerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Answer
+        fields = ["id", "content", "author", "is_adopted", "created_at"]
+
+# 카테고리 목록 조회
+class AdminCategoryListSerializer(serializers.ModelSerializer[QuestionCategory]):
+    type = serializers.CharField(read_only=True)
+
+    class Meta:
+        model = QuestionCategory
+        fields = ["id", "name", "parent", "type", "created_at", "updated_at"]
 
 
 # 질문 목록 조회
@@ -20,13 +33,15 @@ class AdminQuestionListSerializer(serializers.ModelSerializer[Question]):
         fields = ["id", "title", "content", "author", "category", "view_count", "created_at", "updated_at", "images"]
 
 
-# 질문 상세 조회
+# 질의 응답 상세 조회
 class AdminQuestionDetailSerializer(serializers.ModelSerializer[Question]):
     images = AdminQuestionImageSerializer(many=True, read_only=True)
+    answers = AdminAnswerSerializer(many=True, read_only=True)
+    category = AdminCategoryListSerializer(read_only=True)
 
     class Meta:
         model = Question
-        fields = ["id", "title", "content", "author", "category", "view_count", "created_at", "updated_at", "images"]
+        fields = ["id", "title", "content", "author", "category", "view_count", "created_at", "updated_at", "images", "answers", "category"]
 
 
 # 질의응답 카테고리 질문 등록
@@ -42,10 +57,4 @@ class AdminCategoryCreateSerializer(serializers.ModelSerializer[QuestionCategory
     # 검증 된 데이터로 생성 코드 구현 예정
 
 
-# 카테고리 목록 조회
-class AdminCategoryListSerializer(serializers.ModelSerializer[Question]):
-    images = AdminQuestionImageSerializer(many=True, read_only=True)
 
-    class Meta:
-        model = Question
-        fields = ["id", "name", "parent", "type", "created_at", "updated_at"]
