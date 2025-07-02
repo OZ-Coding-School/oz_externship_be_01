5194f8e861be4e1a3259646c0bbcc9ece9ac14af
yeduen <gaurga@naver.com>
2025-07-02 11:22:19 +0900

    feat: StudentEnrollmentRequest 모델을 활용한 과정별 수강 등록 API 추가

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -1,15 +1,18 @@
 from django.urls import path
 
-from apps.courses.views.subject_views import (
-    SubjectDetailAPIView,
-    SubjectListCreateAPIView,
+from apps.courses.views.subject_views import SubjectDetailAPIView, SubjectListCreateAPIView
+from .views.course_views import (
+    CourseDetailView, 
+    CourseListCreateView, 
+    CourseEnrollmentStatsView,
+    CourseEnrollmentListView
 )
-
-from .views.course_views import CourseDetailView, CourseListCreateView
 
 urlpatterns = [
     path("courses/", CourseListCreateView.as_view(), name="v1_admin_course_list"),
     path("courses/<int:course_id>/", CourseDetailView.as_view(), name="v1_admin_course_detail"),
+    path("courses/<int:course_id>/enrollment-stats/", CourseEnrollmentStatsView.as_view(), name="v1_admin_course_enrollment_stats"),
+    path("courses/<int:course_id>/enrollments/", CourseEnrollmentListView.as_view(), name="v1_admin_course_enrollments"),
     path("subjects/", SubjectListCreateAPIView.as_view(), name="subject-list-create"),
     path("subjects/<int:subject_id>/", SubjectDetailAPIView.as_view(), name="subject-detail"),
 ]
