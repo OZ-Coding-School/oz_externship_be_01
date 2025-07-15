54cc9b5381d068655a70bca5a7f5a7abdb7bc025
CJY9697 <jy970114@naver.com>
2025-07-15 11:08:34 +0900

    🐛 fix : 댓글 리스트 테스트코드 추가

--- apps/community/tests/test_comment.py
+++ apps/community/tests/test_comment.py
@@ -16,6 +16,9 @@ class UserCommentTestCase(TestCase):
         self.user = User.objects.create_user(
             email="user@test.com", name="일반유저", nickname="user1", phone_number="01012345678", password="userpass"
         )
+        self.other_user = User.objects.create_user(
+            email="other@test.com", name="다른유저", nickname="other1", phone_number="01087654321", password="otherpass"
+        )
         self.tagged_user = User.objects.create_user(
             email="tagged@test.com", name="태그유저", nickname="tagged1", phone_number="01098765432", password="tagpass"
         )
@@ -29,6 +32,15 @@ class UserCommentTestCase(TestCase):
         )
 
         self.comment = Comment.objects.create(post=self.post, author=self.user, content="기존 댓글입니다")
+
+    def test_comment_list(self):
+        Comment.objects.create(post=self.post, author=self.user, content="댓글1")
+        Comment.objects.create(post=self.post, author=self.user, content="댓글2")
+
+        url = reverse("comment-list", kwargs={"post_id": self.post.id})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data["results"]), 3)
 
     def test_create_comment(self):
         url = reverse("comment-create", kwargs={"post_id": self.post.id})
@@ -66,10 +78,7 @@ class UserCommentTestCase(TestCase):
         self.assertEqual(response.status_code, 400)
 
     def test_update_other_users_comment_fail(self):
-        other_user = User.objects.create_user(
-            email="other@test.com", name="다른유저", nickname="other1", phone_number="01055554444", password="otherpass"
-        )
-        comment_by_other = Comment.objects.create(post=self.post, author=other_user, content="남이 쓴 댓글")
+        comment_by_other = Comment.objects.create(post=self.post, author=self.other_user, content="남이 쓴 댓글")
         url = reverse("comment-update", kwargs={"comment_id": comment_by_other.id})
         data = {"content": "남의 댓글 수정 시도"}
         response = self.client.patch(url, data, format="multipart")
@@ -82,10 +91,7 @@ class UserCommentTestCase(TestCase):
         self.assertFalse(Comment.objects.filter(id=self.comment.id).exists())
 
     def test_delete_other_users_comment_fail(self):
-        other_user = User.objects.create_user(
-            email="other@test.com", name="다른유저", nickname="other1", phone_number="01055554444", password="otherpass"
-        )
-        comment_by_other = Comment.objects.create(post=self.post, author=other_user, content="남이 쓴 댓글")
+        comment_by_other = Comment.objects.create(post=self.post, author=self.other_user, content="남이 쓴 댓글")
         url = reverse("comment-delete", kwargs={"comment_id": comment_by_other.id})
         response = self.client.delete(url)
         self.assertEqual(response.status_code, 403)
